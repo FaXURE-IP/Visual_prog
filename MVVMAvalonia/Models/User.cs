61ad18f7be41fb971c2527923287d2e2742ddfb8
FaXURE-IP <readgme@gmail.com>
2024-03-26 13:12:30 +0700

    8_lab is ready

---
+++ MVVMAvalonia/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MVVMAvalonia.Models.Users
+{
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("address")]
+        public Address Address { get; set; }
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+        [JsonPropertyName("website")]
+        public string Website { get; set; }
+    }
+    internal class Company
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("catchPhrase")]
+        public string CatchPhrase { get; set; }
+        [JsonPropertyName("bs")]
+        public string Bs { get; set; }
+    }
+    public class Geo
+    {
+        [JsonPropertyName("lat")]
+        public double Lat { get; set; }
+        [JsonPropertyName("lng")]
+        public double Lng { get; set; }
+    }
+    public class Address
+    {
+        [JsonPropertyName("street")]
+        public string Street { get; set; }
+        [JsonPropertyName("suite")]
+        public string Suite { get; set; }
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+        [JsonPropertyName("zipcode")]
+        public string Zipcode { get; set; }
+        [JsonPropertyName("geo")]
+        public Geo Geo { get; set; }
+    }
+}
---
+++ MVVMAvalonia/ViewModels/BasePageViewModel.cs
@@ -0,0 +1,74 @@
+﻿using MVVMAvalonia.Models;
+using MVVMAvalonia.Models.Users;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using MVVMAvalonia.Models.PagesModels;
+
+
+namespace MVVMAvalonia.ViewModels.Pages
+{
+    public abstract class BasePageViewModel : ViewModelBase
+    {
+        protected UsersModel usersModel;
+        public string Header => GetName();
+        public abstract string GetName();
+        protected BasePageViewModel(UsersModel users)
+        {
+            this.usersModel = users;
+            this.users = new ObservableCollection<User>();
+            this.users = users.Users;
+        }
+
+        private ObservableCollection<User> users;
+        public ObservableCollection<User> Users
+        {
+            get => users;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref users, value);
+            }
+        }
+    }
+    public class DataTreeViewModel : BasePageViewModel
+    {
+        public override string GetName()
+        {
+            return "DataTreeViewModel";
+        }
+
+        public ObservableCollection<Node> Nodes { get; }
+        public DataTreeViewModel(UsersModel usersModel) : base(usersModel)
+        {
+            Nodes = new ObservableCollection<Node>();
+
+            foreach (var user in usersModel.Users)
+            {
+                Nodes.Add(new Node("ID: " + (user.Id.ToString()), new ObservableCollection<Node>
+                {
+                    new Node("Username: " + user.Username), new Node("Name: " + user.Name),
+                    new Node("Email: " + user.Email), new Node("Phone: " + user.Phone),
+                    new Node("Website: " + user.Website), new Node("Address: ",new ObservableCollection<Node>
+                    {
+                        new Node("City: " + user.Address.City), new Node("Street: " + user.Address.Street),
+                        new Node("Suite: " + user.Address.Suite), new Node("Code: " + user.Address.Zipcode),
+                        new Node("Geo: ", new ObservableCollection<Node>
+                        {
+                            new Node("Lat: " + user.Address.Geo.Lat), new Node("Lon: " + user.Address.Geo.Lng)
+                        })
+                    })
+                }));
+            }
+        }
+    }
+    public class DataGridViewModel : BasePageViewModel
+    {
+        public override string GetName()
+        {
+            return "DataGridViewModel";
+        }
+
+        public DataGridViewModel(UsersModel usersModel) : base(usersModel)
+        {
+        }
+    }
+}
